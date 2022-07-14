19fa81d29d42e5d2b1db63ee41e120b73b279c19
isaiaszanoni <isaiasz.mus@gmail.com>
2022-07-14 10:21:27 -0300

    feat:(front, UseState): Hook use state para datas

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 import NotificationButton from '../NotificationButton';
-
 import './style.css';
 
 function SalesCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
+
     return (
         <>
             <div className="dsmeta-card">
@@ -13,8 +21,8 @@ function SalesCard() {
                         <h2 className="dsmeta-sales-tittle">Vendas</h2>
                         <div className="dsmeta-form-control-container">
                             <DatePicker
-                                selected={new Date()}
-                                onChange={(date: Date) => {}}
+                                selected={minDate}
+                                onChange={(date: Date) => setMinDate(date)}
                                 className="dsmeta-input"
                                 dateFormat="dd/MM/yyyy"
                             />
@@ -22,8 +30,8 @@ function SalesCard() {
                         </div>
                         <div className="dsmeta-form-control-container">
                             <DatePicker
-                                selected={new Date()}
-                                onChange={(date: Date) => {}}
+                                selected={maxDate}
+                                onChange={(date: Date) => setMaxDate(date)}
                                 className="dsmeta-input"
                                 dateFormat="dd/MM/yyyy"
                             />
@@ -85,4 +93,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
