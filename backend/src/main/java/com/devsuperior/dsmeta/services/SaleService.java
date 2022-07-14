6817ca257ee339e20227eda5de958d13c38bcac4
isaiaszanoni <isaiasz.mus@gmail.com>
2022-07-14 14:20:46 -0300

    feat(back): resposta paginada

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -3,8 +3,10 @@ package com.devsuperior.dsmeta.services;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Service
@@ -13,8 +15,8 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
 }
