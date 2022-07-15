5e5d0f9d59b3ae88b252935c89d9eaa69acabecf
isaiaszanoni <isaiasz.mus@gmail.com>
2022-07-15 16:58:29 -0300

    feat(front): adiciona o tosty para mostral um modal quando for notificado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import NotificationButton from "./components/NotificationButton";
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
+import { ToastContainer } from "react-toastify";
 
 function App() {
 
   return (
     <>
+      <ToastContainer />
       <Header />
-      
       <main>
         <section id="sales">
           <div className="dsmeta-container">
